c4e5acfc8559f645ac0b1a953e01d44291a2ccf0
Henry <tosca207@yahoo.com>
2018-11-07 09:15:20 +0100

    update

--- app/src/main/java/com/iamdeejay/todolist/AddToDoActivity.java
+++ app/src/main/java/com/iamdeejay/todolist/AddToDoActivity.java
@@ -3,7 +3,11 @@ package com.iamdeejay.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class AddToDoActivity extends AppCompatActivity {
+
+    private static final String TAG = AddToDoActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
