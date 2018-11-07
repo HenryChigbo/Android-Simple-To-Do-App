4512c18a51cf0aef20949b9c99d582a54e46c0ab
Henry <tosca207@yahoo.com>
2018-11-07 09:53:07 +0100

    update

--- app/src/main/java/com/iamdeejay/todolist/AddToDoActivity.java
+++ app/src/main/java/com/iamdeejay/todolist/AddToDoActivity.java
@@ -1,17 +1,59 @@
 package com.iamdeejay.todolist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.iamdeejay.todolist.model.Todo;
+import com.iamdeejay.todolist.model.TodoDAO;
+import com.iamdeejay.todolist.model.TodoDatabase;
 
 
 public class AddToDoActivity extends AppCompatActivity {
 
+
     private static final String TAG = AddToDoActivity.class.getSimpleName();
+
+
+    TextView title;
+    TextView description;
+    FloatingActionButton fab;
+    Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+
+        title = findViewById(R.id.title);
+
+        description = findViewById(R.id.description);
+
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String todoTitle = title.getText().toString();
+                final String todoDescription = description.getText().toString();
+
+                if (TextUtils.isEmpty(todoTitle) && TextUtils.isEmpty(todoDescription)){
+                    Toast.makeText(context, "Todo Entry cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Todo newTodo = new Todo();
+                    //Add new entry to the list
+                    TodoDAO todoDAO = TodoDatabase.getInstance(context).todo();
+                    todoDAO.insertToDo(newTodo);
+
+                    Toast.makeText(context, "Successfully entered a new Todo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
